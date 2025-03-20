94581437953a5a9f6e1848829c020eb4ef02185c
Sagar Kedar <sagarkedar@Admins-MacBook-Pro.local>
2025-03-20 13:05:08 +0530

    update pipeline groovy

--- jobs/pipeline.groovy
+++ jobs/pipeline.groovy
@@ -1,26 +1,41 @@
-pipeline {
-    agent any
-    environment {
-        KUBECONFIG = "/Users/sagarkedar/.kube/config"
-    }
-    stages {
-        stage('Checkout Code') {
-            steps {
-                git 'https://github.com/your-repo/jenkins-k8s-pipeline.git'
-            }
-        }
+pipelineJob('MyPipelineJob') {
+    description('This is a pipeline job created with Job DSL')   
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+                    environment {
+                        KUBECONFIG = "/Users/sagarkedar/.kube/config"
+                    }
+                    stages {
+                        stage('Checkout Code') {
+                            steps {
+                                script {
+                                    git 'https://github.com/Sagar-kedar/project.git'
+                                }
+                            }
+                        }
 
-        stage('Apply Kubernetes Manifests') {
-            steps {
-                sh 'kubectl apply -f k8s-deployment-svc.yaml'
-            }
-        }
+                        stage('Apply Kubernetes Manifests') {
+                            steps {
+                               script {
+                                   sh 'kubectl apply -f k8s-deployment-svc.yaml'
+                               }
+                            }
+                        }
 
-        stage('Verify Deployment') {
-            steps {
-                sh 'kubectl get pods'
-                sh 'kubectl get svc myapp-service'
-            }
+                        stage('Verify Deployment') {
+                            steps {
+                               script {
+                                   sh 'kubectl get pods'
+                                   sh 'kubectl get svc myapp-service'
+                               }
+                            }
+                        }
+                    }
+                }
+            """)
         }
     }
-}
+}                
